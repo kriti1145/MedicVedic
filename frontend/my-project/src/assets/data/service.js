121903880dd12dc042a7f7228d1a3d3280320c96
Kriti <1910004@sliet.ac.in>
2024-06-26 22:45:44 +0530

    Create service.js

---
+++ frontend/my-project/src/assets/data/service.js
@@ -0,0 +1,44 @@
+export const services= [
+  {
+    name: "Cancer Care",
+    desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+    bgColor: "rgba(254, 182, 13, .2)",
+    textColor: "#FEB60D",
+     ServiceImg: "/src/assets/Images/servicess/lung-cancer.png"
+  },
+  {
+    name: "Labor & Delivery",
+    desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+    bgColor: "rgba(151, 113, 255, .2)",
+    textColor: "#9771FF",
+     ServiceImg: "/src/assets/Images/servicess/baby.png"
+  },
+  {
+    name: "Heart & Vascular",
+    desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+    bgColor: "rgba(1, 181, 197, .2)",
+    textColor: "#01B5C5",
+     ServiceImg:"/src/assets/Images/servicess/heart.png"
+  },
+  {
+    name: "Mental Health",
+    desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+    bgColor: "rgba(1, 181, 197, .2)",
+    textColor: "#01B5C5",
+     ServiceImg: "/src/assets/Images/servicess/mental.png"
+  },
+  {
+    name: "Neurology",
+    desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+    bgColor: "rgba(254, 182, 13, .2)",
+    textColor: "#FEB60D",
+     ServiceImg: "/src/assets/Images/servicess/neuron.png"
+  },
+  {
+    name: "Burn Treatment",
+    desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+    bgColor: "rgba(151, 113, 255, .2)",
+    textColor: "#9771FF",
+    ServiceImg: "/src/assets/Images/servicess/burn.png"
+  },
+];
